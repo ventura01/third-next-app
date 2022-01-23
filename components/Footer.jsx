6569de83960a9c03c2607f1393e59f19c860ca46
rafael <rventu@gmail.com>
2022-01-22 20:41:57 -0600

    Antes de conectar MongoDB

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+const year = new Date().getFullYear();
+const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+const month = new Date().getMonth();
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <p>
+        Todos los derechos reservados ©{months[month]} / {year}
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
