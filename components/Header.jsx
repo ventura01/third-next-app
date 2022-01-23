d54d614d895b202d01ad6f42cf883a9c6e8a3853
rafael <rventu@gmail.com>
2022-01-22 19:07:02 -0600

    Agregado estilos CSS

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,39 @@
 import styles from "../styles/Header.module.css";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className={styles.Header}>
+    <div>
       <div className="row">
-        <div className="col-md-6">
-          <h1 className="titulo">Menos es mas</h1>
-          <p className="texto">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            eos ea facilis, ipsa nihil porro maxime pariatur, incidunt ex iste,
-            reprehenderit obcaecati consectetur iusto eum sit. Eius similique
-            exercitationem praesentium inventore, animi rem corrupti, provident
-            quaerat voluptas quasi fuga saepe nemo quisquam suscipit ex? Nulla,
-            sunt. Iste beatae quas fugiat optio facere, unde ipsam.
-          </p>
+        <div className="col-md-4">
+          <div className="textos_header_cont">
+            <h1 className={styles.titulo}>Menos es más...</h1>
+            <p className={styles.sub_titulo}>
+              Muchos elementos cargan al buen diseño!
+            </p>
+            <p className={styles.texto}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+              eos ea facilis, ipsa nihil porro maxime pariatur, incidunt ex
+              iste, reprehenderit obcaecati consectetur iusto eum sit. Eius
+              similique exercitationem praesentium inventore, animi rem
+              corrupti, provident quaerat voluptas quasi fuga saepe nemo
+              quisquam suscipit ex? Nulla, sunt. Iste beatae quas fugiat optio
+              facere, unde ipsam.
+            </p>
+          </div>
         </div>
-        <div className="col-md-6"></div>
+        <div className="col-md-8">
+          <div className="image_header_cont">
+            <Image
+              src="/img/3.jpg"
+              width={1920}
+              height={1280}
+              alt="Image 1"
+              priority
+              layout="responsive"
+            ></Image>
+          </div>
+        </div>
       </div>
     </div>
   );
